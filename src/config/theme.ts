32eb68ee9de32e8d9fcd41cb14f20280f3c8404a
serkan-uslu <serkanuslu.info@gmail.com>
2023-07-15 18:07:55 +0300

    .prettierrc file was added to project

--- src/config/theme.ts
+++ src/config/theme.ts
@@ -1,17 +1,17 @@
-import { createTheme } from "@mui/material/styles";
-import { red } from "@mui/material/colors";
+import { createTheme } from '@mui/material/styles'
+import { red } from '@mui/material/colors'
 // Create a theme instance.
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#556cd6",
+    palette: {
+        primary: {
+            main: '#556cd6',
+        },
+        secondary: {
+            main: '#19857b',
+        },
+        error: {
+            main: red.A400,
+        },
     },
-    secondary: {
-      main: "#19857b",
-    },
-    error: {
-      main: red.A400,
-    },
-  },
-});
-export default theme;
+})
+export default theme
