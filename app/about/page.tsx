296d368dc1de0bec0867cb2a33b4cdf5dcb1bab0
serkan-uslu <serkanuslu.info@gmail.com>
2024-01-12 20:14:56 +0300

    chore: delete unused files

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,8 @@
+import AuthorLayout from '@/layouts/AuthorLayout'
+import { genPageMetadata } from 'app/seo'
 import { Authors, allAuthors } from 'contentlayer/generated'
 import { MDXLayoutRenderer } from 'pliny/mdx-components'
-import AuthorLayout from '@/layouts/AuthorLayout'
 import { coreContent } from 'pliny/utils/contentlayer'
-import { genPageMetadata } from 'app/seo'
-// import ToolList from '@/data/tools'
-// import Tools from '@/app/Tools'
 
 export const metadata = genPageMetadata({ title: 'About' })
 
@@ -16,7 +14,6 @@ export default function Page() {
     <>
       <AuthorLayout content={mainContent}>
         <MDXLayoutRenderer code={author.body.code} />
-        {/* <Tools tools={ToolList} /> */}
       </AuthorLayout>
     </>
   )
--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,10 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'My Website',
+    description: `My website is finally complete and ready to go live. After a long design and development process, seeing the results is incredibly exciting.`,
+    imgSrc: '/serkan-uslu.png',
+    href: 'https://github.com/serkan-uslu/serkanuslu.com',
   },
 ]
 
