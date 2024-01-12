73a169ba20c7f78825cc6c6a339bf947177d43e6
Serkan USLU <65805346+serkan-uslu@users.noreply.github.com>
2024-01-12 15:54:03 +0300

    fix: change navigation order

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,10 +1,10 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/about', title: 'About' },
-  { href: 'https://medium.com/@serkan-uslu', title: 'Blog' },
   { href: '/projects', title: 'Projects' },
   // { href: '/blog', title: 'Bookmarks' },
   // { href: '/tags', title: 'Tags' },
+  { href: 'https://medium.com/@serkan-uslu', title: 'Blog' },
   { href: './cv-serkanuslu.pdf', title: 'My Resume' },
 ]
 
