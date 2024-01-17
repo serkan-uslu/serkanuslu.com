f01e119be436f1b699400b3cdaf727ae145cba41
serkan-uslu <serkanuslu.info@gmail.com>
2024-01-17 22:24:05 +0300

    fix: about page was updated

--- components/Tools.tsx
+++ components/Tools.tsx
@@ -1,9 +1,19 @@
+'use client'
+
 import Image from 'next/image'
+import { useParams, usePathname } from 'next/navigation'
+import { LocaleTypes } from 'app/[locale]/i18n/settings'
+import { useTranslation } from 'app/[locale]/i18n/client'
 
 export default function Tools({ tools }) {
+  const locale = useParams()?.locale as LocaleTypes
+  const { t } = useTranslation(locale, 'about')
+
   return (
     <>
-      <h4 className="mb-4 text-center text-xl font-semibold md:text-left">Languages and Tools</h4>
+      <h4 className="mb-4 text-center text-xl font-semibold md:text-left">
+        {t('languagesAndTools')}
+      </h4>
       <div className="flex flex-wrap gap-4">
         {tools.map((item, index) => (
           <a
